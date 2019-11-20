2e7f6e3ff33189c29931c7a7f6ad10ebf6f941f5
dietrich <dietrich@newskylabs.net>
2019-11-20 16:19:25 +0100

    Added command line options for image dir, class mapping file, and annotation file

--- newskylabs/tools/scissors/scripts/scissors.py
+++ newskylabs/tools/scissors/scripts/scissors.py
@@ -46,16 +46,50 @@ from newskylabs.tools.scissors.utils.generic import get_version_long
 ## Main
 ## ---------------------------------------------------------
 
+# -i, --image-dir
+option_image_dir_help = "Directory with the text images."
+option_image_dir_default = "data/images" # TODO Get the default data dir from the config.
+
+# -m, --class-mapping
+option_class_mapping_help = "A file mapping class labels to actual values."
+option_class_mapping_default = "data/class-mappings.csv" # TODO Get the default data mapping file from the config.
+
+# -a, --annotations
+option_annotations_help = "A file with the character annotations (labels and bounding boxes)."
+option_annotations_default = "data/annotations.csv" # TODO Get the default annotation file from the config.
+
 @click.command()
+
+@click.option('-i', '--image-dir',
+              type=click.Path(exists=True), 
+              default=option_image_dir_default,
+              help=option_image_dir_help)
+
+@click.option('-m', '--class-mapping',
+              type=click.Path(exists=True), 
+              default=option_class_mapping_default,
+              help=option_class_mapping_help)
+
+@click.option('-a', '--annotations',
+              type=click.Path(exists=False), 
+              default=option_annotations_default,
+              help=option_annotations_help)
 
 @click.version_option(get_version_long(), '-V', '--version')
 
-def scissors():
+def scissors(image_dir, class_mapping, annotations):
     """A tool for recognizing and editing character bounding boxes and
     class annotations.
 
     """
     print("Hello Click Scissors :)")
+
+    # DEBUG
+    print("")
+    print("  - image_dir:     {}".format(image_dir))
+    print("  - class_mapping: {}".format(class_mapping))
+    print("  - annotations:   {}".format(annotations))
+    print("")
     
 ## =========================================================
 ## =========================================================
